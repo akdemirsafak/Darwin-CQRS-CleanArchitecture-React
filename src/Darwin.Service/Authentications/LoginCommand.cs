cbfd7aec614551fae86a4699b52de16ad917cbde
akdemirsafak <akdemirsafak@gmail.com>
2023-09-20 17:54:51 +0300

    jwttoken operations completed. logincommand and registercommand returning token

--- src/Darwin.Service/Authentications/LoginCommand.cs
+++ src/Darwin.Service/Authentications/LoginCommand.cs
@@ -4,11 +4,12 @@ using Darwin.Core.Entities;
 using Darwin.Model.Common;
 using Darwin.Model.Request.Authentications;
 using Darwin.Service.Common;
+using Darwin.Service.TokenOperations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Darwin.Service.Authentications;
 
-public class LoginCommand:ICommand<DarwinResponse<NoContent>>
+public class LoginCommand:ICommand<DarwinResponse<TokenResponse>>
 {
     public LoginRequest Model { get;}
 
@@ -17,35 +18,41 @@ public class LoginCommand:ICommand<DarwinResponse<NoContent>>
         Model = model;
     }
 
-    public class Handler : ICommandHandler<LoginCommand, DarwinResponse<NoContent>>
+    public class Handler : ICommandHandler<LoginCommand, DarwinResponse<TokenResponse>>
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        private readonly ITokenService _tokenService;
+        public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _tokenService = tokenService;
         }
 
-        public async Task<DarwinResponse<NoContent>> Handle(LoginCommand request, CancellationToken cancellationToken)
+        public async Task<DarwinResponse<TokenResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
             var existUser= await _userManager.FindByNameAsync(request.Model.UserName);
             if (existUser is null)
             {
-                return DarwinResponse<NoContent>.Fail("User Not Found.", 400);
+                return DarwinResponse<TokenResponse>.Fail("User Not Found.", 400);
             }
             var loginResult= await _signInManager.CheckPasswordSignInAsync(existUser,request.Model.Password,true);
             if (loginResult.IsLockedOut)
             {
-                return DarwinResponse<NoContent>.Fail("User locked.Try again 5 min.", 400);
+                return DarwinResponse<TokenResponse>.Fail("User locked.Try again 5 min.", 400);
             }
             if (!loginResult.Succeeded)
             {
-                return DarwinResponse<NoContent>.Fail("Login Failed.", 400);
+                return DarwinResponse<TokenResponse>.Fail("Login Failed.", 400);
             }
             existUser.LastLogin = DateTime.UtcNow.Ticks;
-            await _userManager.UpdateAsync(existUser);
-            return DarwinResponse<NoContent>.Success(200);
+            var updateResult=await _userManager.UpdateAsync(existUser);
+            if(!updateResult.Succeeded)
+            {
+                return DarwinResponse<TokenResponse>.Fail("Login cannot updated.", 500); //Refactor
+            }
+            return DarwinResponse<TokenResponse>.Success(_tokenService.CreateToken(existUser),200);
         }
     }
 }
--- src/Darwin.Service/Authentications/RegisterCommand.cs
+++ src/Darwin.Service/Authentications/RegisterCommand.cs
@@ -3,11 +3,12 @@ using Darwin.Core.Entities;
 using Darwin.Model.Common;
 using Darwin.Model.Request.Authentications;
 using Darwin.Service.Common;
+using Darwin.Service.TokenOperations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Darwin.Service.Authentications;
 
-public class RegisterCommand : ICommand<DarwinResponse<NoContent>>
+public class RegisterCommand : ICommand<DarwinResponse<TokenResponse>>
 {
     public RegisterRequest Model { get; }
 
@@ -16,32 +17,35 @@ public class RegisterCommand : ICommand<DarwinResponse<NoContent>>
         Model = model;
     }
 
-    public class Handler : ICommandHandler<RegisterCommand, DarwinResponse<NoContent>>
+    public class Handler : ICommandHandler<RegisterCommand, DarwinResponse<TokenResponse>>
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public Handler(UserManager<AppUser> userManager)
+        public Handler(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
-        public async Task<DarwinResponse<NoContent>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        public async Task<DarwinResponse<TokenResponse>> Handle(RegisterCommand request, CancellationToken cancellationToken)
         {
             var hasUser= await _userManager.FindByNameAsync(request.Model.UserName);
             if(hasUser is not null)
             {
-                return DarwinResponse<NoContent>.Fail("Already exist Username.", 400);
+                return DarwinResponse<TokenResponse>.Fail("Already exist Username.", 400);
             }
             if (request.Model.Password != request.Model.PasswordAgain)
             {
-                return DarwinResponse<NoContent>.Fail("Passwords do not match.", 400);
+                return DarwinResponse<TokenResponse>.Fail("Passwords do not match.", 400);
             }
-            var registerResult= await _userManager.CreateAsync(new AppUser{UserName=request.Model.UserName},request.Model.Password);
+            AppUser newUser=new AppUser{UserName=request.Model.UserName};
+            var registerResult= await _userManager.CreateAsync(newUser,request.Model.Password);
             if (!registerResult.Succeeded)
             {
-                return DarwinResponse<NoContent>.Fail(new List<string> { $"Email veya şifre yanlış.", $"Başarısız giriş sayısı : {registerResult.Errors.SelectMany(x=>x.Description).ToList()}"});
+                return DarwinResponse<TokenResponse>.Fail(new List<string> { $"Email veya şifre yanlış.", $"Başarısız giriş sayısı : {registerResult.Errors.SelectMany(x=>x.Description).ToList()}"});
             }
-            return DarwinResponse<NoContent>.Success(201);
+            return DarwinResponse<TokenResponse>.Success(_tokenService.CreateToken(newUser),201);
         }
     }
 }
