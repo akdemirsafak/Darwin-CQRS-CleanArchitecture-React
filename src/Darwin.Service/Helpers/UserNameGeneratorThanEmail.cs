ee956fcc5a46f294f53cddbf5840a5987b2c96d6
akdemirsafak <akdemirsafak@gmail.com>
2023-09-23 13:42:33 +0300

    Generated.

---
+++ src/Darwin.Service/Helpers/UserNameGeneratorThanEmail.cs
@@ -0,0 +1,12 @@
+﻿namespace Darwin.Service.Helpers;
+
+public static class UserNameGeneratorThanEmail
+{
+    public static string Generate(string email)
+    {
+        var atIndex = email.IndexOf("@");
+        email = email.Substring(0, atIndex);
+        var random = new Random();
+        return email + random.Next(1234, 9876);
+    }
+}
