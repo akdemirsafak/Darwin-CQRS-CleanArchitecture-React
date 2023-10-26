2be6228fccec9be3ec40b74c62e36075aa4fddc3
akdemirsafak <akdemirsafak@gmail.com>
2023-10-26 17:15:35 +0300

    vuetify installation with materialdesign icons

--- ui/webui/app.vue
+++ ui/webui/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello</h1>
+    <v-btn color="primary" prepend-icon="mdi-pencil" >
+      This is a Button
+    </v-btn>
+   
   </div>
 </template>
--- ui/webui/nuxt.config.ts
+++ ui/webui/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css:[
+    'vuetify/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+    build:{
+      transpile:['vuetify'],
+    },
+      devtools: { enabled: true }
 })
