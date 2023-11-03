6a2ac25e1f70ecb31f6307f781579fae1ee98388
akdemirsafak <akdemirsafak@gmail.com>
2023-11-03 13:41:50 +0300

    Kullanıcı kayıt olduğunda mesaj gönderilir hale getirildi.

---
+++ src/Darwin.Service/Notifications/UserCreated/UserCreatedEvent.cs
@@ -0,0 +1,22 @@
+﻿using Darwin.Core.Entities;
+using Darwin.Service.EmailServices;
+using MediatR;
+
+namespace Darwin.Service.Notifications.UserCreated;
+
+public record UserCreatedEvent(UserCreatedMailModel userCreatedMailModel) : INotification;
+
+internal sealed class UserCreatedEventHandler : INotificationHandler<UserCreatedEvent>
+{
+    private readonly IEmailService _emailService;
+
+    public UserCreatedEventHandler(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    public async Task Handle(UserCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        await _emailService.SendWellcomeWithConfirmationAsync(notification.userCreatedMailModel);
+    }
+}
