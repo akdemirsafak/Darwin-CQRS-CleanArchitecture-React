73b3d056e8aefa8b689a785d8d1f03b74750dc3d
akdemirsafak <akdemirsafak@gmail.com>
2024-01-10 12:20:00 +0300

    Installed stackexchange packages. Redis Cache added. Pipeline created and

---
+++ src/Darwin.Service/Configures/RedisCacheSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Darwin.Service.Configures;
+
+public class RedisCacheSettings
+{
+    public string ConnectionString { get; set; }
+    public string InstanceName { get; set; } //Redis Database Name
+}
