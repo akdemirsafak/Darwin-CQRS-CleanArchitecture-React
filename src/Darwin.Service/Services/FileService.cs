d95e234d8714b435c3f21d41e800a2c3dd1f5504
akdemirsafak <akdemirsafak@gmail.com>
2024-01-12 21:31:01 +0300

    File Guid Name Upgrade

--- src/Darwin.Service/Services/FileService.cs
+++ src/Darwin.Service/Services/FileService.cs
@@ -8,10 +8,12 @@ public class FileService : IFileService
     {
         if (image is not null && image.Length > 0)
         {
-            var path=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",image.FileName);
+            var extent = Path.GetExtension(image.FileName);
+            var randomName = ($"{Guid.NewGuid()}{extent}");
+            var path=Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",randomName);
             using var stream=new FileStream(path,FileMode.Create);
             await image.CopyToAsync(stream, CancellationToken.None);
-            return image.FileName;
+            return randomName;
         }
         return null;
     }
