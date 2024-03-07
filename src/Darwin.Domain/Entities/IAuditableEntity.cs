341f5385b4ea0abc9e37fd647d57705c7a8e2de5
akdemirsafak <akdemirsafak@gmail.com>
2024-03-08 00:31:42 +0300

    Created and entities added.

---
+++ src/Darwin.Domain/Entities/IAuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Darwin.Domain.Entities;
+
+public interface IAuditableEntity
+{
+    DateTime CreatedOnUtc { get; set; }
+    string? CreatedBy { get; set; }
+    DateTime? UpdatedOnUtc { get; set; }
+    string? UpdatedBy { get; set; }
+}
