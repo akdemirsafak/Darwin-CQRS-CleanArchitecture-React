0c2885265a38406607258d5fa07c72dd5e74f772
akdemirsafak <akdemirsafak@gmail.com>
2024-03-15 11:32:32 +0300

    mui added and tested in categorylistitem.

--- ui/darwinreact/src/components/Category/CategoryListItem.js
+++ ui/darwinreact/src/components/Category/CategoryListItem.js
@@ -1,13 +1,15 @@
-
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 export default function CategoryListItem(props){
     return(
             <tr>
                 <td>{props.categoryItem.id}</td>
                 <td>{props.categoryItem.name}</td>
                 <td>{props.categoryItem.isUsable.toString()}</td>
-                <td><img src={props.categoryItem.imageUrl} className="" width={64}/></td>
-                <td><button className="btn btn-outline-warning">Update</button></td>
-                <td><button className="btn btn-outline-danger">Delete</button></td>
+                <td><img src={props.categoryItem.imageUrl} className="" width={64} height={48}/></td>
+                <td><Button variant='outlined'  size='small' color='inherit' startIcon={<EditIcon/>}>Update</Button></td>
+                <td><Button variant='outlined' color='error' size='small' startIcon={<DeleteIcon />}>Delete</Button></td>
             </tr>
        /* { <div className='container'>
             <p>{props.categoryItem.id}</p>
