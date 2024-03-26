f8e46490a249ab64ca8afa0dc5facf87169a70d2
akdemirsafak <akdemirsafak@gmail.com>
2024-03-26 17:43:24 +0300

    moods updated

--- ui/darwinreact/src/pages/Mood/index.js
+++ ui/darwinreact/src/pages/Mood/index.js
@@ -1,9 +1,10 @@
 import {Outlet} from "react-router-dom";
+import {Container} from "@mui/material";
 export default function MoodLayout(){
 
     return(
-        <>
+        <Container maxWidth='xl'>
             <Outlet/>
-        </>
+        </Container>
     )
 }
