17f8216909c53880bdd350347d0386ceb0029ac1
akdemirsafak <akdemirsafak@gmail.com>
2024-03-29 15:35:58 +0300

    Logo updated homelayout paper added.

--- ui/darwinreact/src/pages/HomeLayout.js
+++ ui/darwinreact/src/pages/HomeLayout.js
@@ -1,12 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "../components/Navbar";
+import {Paper} from "@mui/material";
 export default function HomeLayout(){
     return(
-      <>
-
+      <Paper p={2} elevation={3}>
+          
           <Navbar></Navbar>
           <Outlet />
 
-      </>
+      </Paper>
     )
 }
